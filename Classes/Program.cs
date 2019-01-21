34796b7db862fbd2795339c0b693624faca3f4de
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-21 13:25:44 -0500

    Comments Added
    Added Comments for your review.

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -6,6 +6,7 @@
 
         public void Introduce(string to)
         {
+            // cw {TAB} => System.Console.WriteLine(........)
             System.Console.WriteLine("Hi {0}, I am {1}", to, Name);
         }
 
@@ -13,6 +14,19 @@
 
     class Program
     {
+        /*
+         * All Programs have a static Main Function that accepts an array of arguments
+         * 
+         * In our static main we have a new instance of the Person class.  Note the 
+         * variable (var) is in Camel Case.  This is not clear because the name person is
+         * only one word.  however it could have been personThing1 = new Person()
+         * camelCase is used for naming variables.  
+         * 
+         * Also note the different types of comments.  A single line is indicated by //
+         * You can use the Keyboard shortcut CTRL + K + C to manage comments.  Click
+         * anywhere in an individual line and use the shortcut to comment out just the one line
+         * or highlight a series of lines and use the same shortcut to comment out all of the lines.
+         */
         static void Main(string[] args)
         {
             var person = new Person();
