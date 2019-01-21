febb1d474bf36c404914cba553f76af7689d878a
nwilkinson97 <nwilkinson97@users.noreply.github.com>
2019-01-21 12:57:18 -0500

    Create New Method
    In the Person class create a method which will be used to Introduce you to the Person.  In this you can use the keyboard shortcuts pub {TAB} for public and cw {TAB} for Console.WriteLine...

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -3,6 +3,12 @@
     public class Person
     {
         public string Name;
+
+        public void Introduce(string to)
+        {
+            System.Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+
     }
 
     class Program
